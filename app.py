1e4c14495cbfe713acaeb924ddb616a9e35ae354
nshwatal <20175877+nshwatal@users.noreply.github.com>
2021-09-18 15:35:37 -0400

    Form and Navigation

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ import display
 import form
 
 
-on_display = True
-on_form = False
-
 st.sidebar.title('Save West Garage')
 
 pages = {"Form":form,"Display":display}
 
-choice = st.sidebar.selectbox("Navigation",["Form","Display"])
-
+st.sidebar.write("\n")
+choice = st.sidebar.selectbox("Navigation",["Display","Form"])
 pages[choice].app()
 
--- form.py
+++ form.py
@@ -3,6 +3,22 @@ import streamlit as st
 def app():
     st.title("Form")
     with st.form(key='my_form'):
-        text_input = st.text_input(label='Enter some text')
+        event_title = st.text_input(label='Event Title:')
+        event_description = st.text_area(label = "Event Description:", height = 24)
+        
+        
+        leftcol,rightcol = st.columns(2)
+
+        
+       
+        
+        dorm = leftcol.selectbox("Your Dorm:",["Baker","McCormick","Masseh","MacGregor","Burton Conner","Next","New","East Campus","Random","Simmons","West Garage","FSILG","Off Campus","Other"])
+
+
+        status = rightcol.selectbox("Who is invited?",["WG Residents Only", "All MIT Students","Other"])
+        
+
         submit_button = st.form_submit_button(label='Submit')
+
+        
 
