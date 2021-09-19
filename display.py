c70beed3342b83586059d31f8e2e8648d95d877d
unknown <mintychocolate101@gmail.com>
2021-09-18 21:54:33 -0400

    Update

--- display.py
+++ display.py
@@ -8,25 +8,34 @@ df = pd.DataFrame({
     "Description": pd.Series(["East Side Party but WG", "beep beep in west garage", "Joint with Next Big Thing"]),
     "Dorm": pd.Categorical(["East Campus", "East Campus", "Next"]), 
     "Invite": pd.Categorical(["All MIT Students", "WG Residents Only", "WG Residents Only"]), 
-    "Votes": 0
+    "Votes": [0, 1, -2]
 })
-def like(i):
-    st.balloons()
-    df.loc[i, "Votes"] = df.loc[i].Votes + 1
+df = df.sort_values(by="Votes", ascending=False)
+df = df.reset_index(drop=True)
+
+def like(i, increment):
+    global df
+    if increment > 0:
+        st.balloons()
+    df.loc[i, "Votes"] = df.loc[i].Votes + increment
 
 def app():
     st.title("Display Posts")
     st.write('''---''')
+    containers = []
     for i in range(0, len(df)):
-        # container = st.container()
-        # container.write(df.loc[i].Title)
-        with st.container(): # st.expander(container):
+        containers.append(st.empty())
+    for i in range(0, len(df)):
+        with containers[i].container(): 
             st.header(df.loc[i].Title)
             st.subheader(df.loc[i].Description)
             col1, col2, col3 = st.columns(3)
             col2.caption("Event idea from " + df.loc[i].Dorm)
             col1.caption(df.loc[i].Invite)
-            if st.button("Upvote", key=i):
-                like(i)
-            st.write(df.loc[i].Votes)
+            col1, col2, col3, col4 = st.columns([1, 1, 1, 3])
+            if col2.button("Upvote", key=2*i):
+                like(i, 1)
+            if col3.button("Downvote", key=2*i + 1):
+                like(i, -1)
+            col1.subheader("Votes: " + str(df.loc[i].Votes))
             st.write('''---''')
